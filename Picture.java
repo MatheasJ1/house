0f39472e834fdc83a30d6ef481b425853b7cd39a
matheas <matheasjarrouj@gmail.com>
2025-08-29 10:23:51 -0400

    Modified the drawing until question 17 thn did the sunset with the moveVertical method

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
     private boolean drawn;
 
     /**
@@ -26,6 +27,7 @@ public class Picture
         window = new Square();
         roof = new Triangle();  
         sun = new Circle();
+        
         drawn = false;
     }
 
@@ -55,8 +57,9 @@ public class Picture
             sun.moveHorizontal(100);
             sun.moveVertical(-40);
             sun.changeSize(80);
-            sun.makeVisible();
-            drawn = true;
+            
+            
+            
         }
     }
 
